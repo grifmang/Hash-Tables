d36609d70f3a59e44b8ade86c133ca9df9c0caf2
Tim Griffith <grifmang@gmail.com>
2020-05-07 20:33:59 -0400

    histo done 3/3

--- applications/histo/histo.py
+++ applications/histo/histo.py
@@ -1,1 +1,22 @@
-# Implement me.
+import sys
+sys.path.append('../word_count')
+from word_count import word_count
+
+def histo(cache={}):
+    with open('robin.txt', 'r') as file:
+        lines = file.readlines()
+        for line in lines:
+            words = word_count(line)
+            for word in words.items():
+                if word[0] in cache:
+                    cache[word[0]] += 1
+                else:
+                    cache[word[0]] = 1
+    
+    items = list(cache.items())
+    items.sort(key=lambda e: e[1], reverse=True)
+
+    for item in items:
+        print(item[0] + ':' + "#" * item[1])
+
+histo()
